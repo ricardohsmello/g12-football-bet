b5b193d6819b41a51034297d1b24189abbf7a55a
ricardo <ricardohsmello@gmail.com>
2023-04-09 20:15:10 -0300

    adjusting

--- src/main/java/br/com/ricas/infrastructure/repository/ScoreBoardRepository.java
+++ src/main/java/br/com/ricas/infrastructure/repository/ScoreBoardRepository.java
@@ -6,6 +6,5 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class ScoreBoardRepository implements PanacheRepositoryBase<ScoreBoardEntity,Integer> {
-
+public class ScoreBoardRepository implements PanacheRepositoryBase<ScoreBoardEntity, Integer> {
 }
