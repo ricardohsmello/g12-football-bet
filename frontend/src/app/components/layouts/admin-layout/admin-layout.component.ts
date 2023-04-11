b3d3ac77c7b229ddb65469f0af89658f2f4c8ffb
ricardo <ricardohsmello@gmail.com>
2023-04-10 22:07:57 -0300

    refactor

--- frontend/src/app/components/layouts/admin-layout/admin-layout.component.ts
+++ frontend/src/app/components/layouts/admin-layout/admin-layout.component.ts
@@ -2,8 +2,7 @@ import {filter} from 'rxjs/operators';
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Location, LocationStrategy, PathLocationStrategy, PopStateEvent } from '@angular/common';
 
-import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+ import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 import { Subscription ,  Observable } from 'rxjs';
 import PerfectScrollbar from 'perfect-scrollbar';
 
--- frontend/src/app/components/layouts/admin-layout/admin-layout.module.ts
+++ frontend/src/app/components/layouts/admin-layout/admin-layout.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../domain/user-profile/user-profile.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { ChartsModule } from 'ng2-charts';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
-import { TeamListComponent } from '../../domain/user-profile/team-list/team-list.component';
-
+import { ScoreBoardComponent } from '../../score-board/score-board.component';
+import { TeamListComponent } from '../../team-list/team-list.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+ 
 @NgModule({
   imports: [
     CommonModule,
@@ -25,6 +26,7 @@ import { TeamListComponent } from '../../domain/user-profile/team-list/team-list
     UserProfileComponent,
     NotificationsComponent,
     TeamListComponent,
+    ScoreBoardComponent
   ]
 })
 
