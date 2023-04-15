de32d460e073c1f232db9f60498fc94221bec3ac
ricardo <ricardohsmello@gmail.com>
2023-04-15 15:22:48 -0300

    match response

---
+++ src/main/java/br/com/ricas/application/web/response/MatchResponse.java
@@ -0,0 +1,11 @@
+package br.com.ricas.application.web.response;
+
+public class MatchResponse {
+
+    private String id;
+    private String teamA;
+    private String teamB;
+    private String date;
+    private String status;
+
+}
